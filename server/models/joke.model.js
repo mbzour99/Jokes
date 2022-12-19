6c892fa0ff231deb19cccc77b7d0fb7c57c93556
mohammad <m.albzour99@gmail.com>
2022-12-19 04:26:34 -0800

    first commit

---
+++ server/models/joke.model.js
@@ -0,0 +1,11 @@
+
+const mongoose = require("mongoose");
+const JokeSchema = new mongoose.Schema({
+	panchline: String,
+	setup: String
+});
+
+const Joke = mongoose.model("Joke", JokeSchema);
+
+module.exports = Joke;
+
